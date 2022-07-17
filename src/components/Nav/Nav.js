1f1a154a0f3697a62a9bc3e1fd34189d488cd666
ronBP95 <ronmagpantay9513@gmail.com>
2022-07-17 16:18:09 -0700

    added file structure and built out nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <div>
+                <p>Logo</p>
+            </div>
+            <ul>
+                <li>01. Home</li>
+                <li>02. About</li>
+                <li>03. Work</li>
+                <li>04. Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
