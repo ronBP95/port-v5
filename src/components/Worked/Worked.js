436b3e8a74abbd9a586bb5556dd1138d718b8ae1
ronBP95 <ronmagpantay9513@gmail.com>
2022-07-25 11:10:15 -0700

    viewport changes to width and height for worked + about component

--- src/components/Worked/Worked.js
+++ src/components/Worked/Worked.js
@@ -4,7 +4,24 @@ import './Worked.css'
 const Worked = () => {
     return (
         <div className='worked'>
-            <p>Worked</p>
+            <div className='workedHeader'>
+                <p><span id="workednum">02.</span> Where I've Worked</p>
+                <div id='workline'></div>
+            </div>
+            <br></br>
+            <div className='workedContent'>
+                <div className='workedNav'>
+                    <ul>
+                        <li>nav</li>
+                        <li>nav</li>
+                        <li>nav</li>
+                        <li>nav</li>
+                    </ul>
+                </div>
+                <div className='workedJobs'>
+                    <p>Here is where the content will go</p>
+                </div>
+            </div>
         </div>
     )
 }
