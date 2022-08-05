e02fbd5aa927905215b639cf71777be77aa723fd
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-04 20:39:19 -0700

    updated the codepen nav item to the proper URL

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -8,7 +8,7 @@ const Social = () => {
             <ul className='socials'>
                 <a id="github" href="hhttps://github.com/ronBP95" target="_blank" rel="noreferrer noopener"><FiGithub/></a>
                 <a id="linkedin" href="https://www.linkedin.com/in/ron-magpantay/" target="_blank" rel="noreferrer noopener"><FiLinkedin /></a>
-                <a id="codepen" href="http://www.codepen.io" target="_blank" rel="noreferrer noopener"><FiCodepen /></a>
+                <a id="codepen" href="https://codepen.io/ronBP95" target="_blank" rel="noreferrer noopener"><FiCodepen /></a>
                 <div id="vl"></div>
             </ul>
         </div>
