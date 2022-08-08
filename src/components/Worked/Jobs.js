364dc2cb8a32bd33a6f3faac8e91f60064b07c90
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-07 18:03:53 -0700

    Created css for mdx files

--- src/components/Worked/Jobs.js
+++ src/components/Worked/Jobs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql, useStaticQuery } from "gatsby";
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import './Jobs.css'
 
 const Jobs = (props) => {
     const data = useStaticQuery(graphql`
@@ -21,14 +22,14 @@ const Jobs = (props) => {
     let node = data.allMdx.nodes[props.data]
     console.log(props.data + "is here")
     return (
-      <div>
+      <div className='jobPage'>
         <div>
-          <p>{node.frontmatter.position} @ {node.frontmatter.title} </p>
+          <p>{node.frontmatter.position} <span className='companyTitle'>@ {node.frontmatter.title}</span> </p>
         </div>
-        <div>
+        <div className='jobDate'>
           {node.frontmatter.date}
         </div>
-        <div>
+        <div className='jobDesc'>
           <MDXRenderer>{node.body}</MDXRenderer>
         </div>
       </div>  
