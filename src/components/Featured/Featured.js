e5ecff150f04c84de64a165c1a3b1c92b09da199
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-08 21:49:13 -0700

    HTML + CSS structure built out for the Featured component

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -7,6 +7,48 @@ const Featured = () => {
             <div className='featuredHeader'>
                 <p>03. Some Things I've Worked On</p>
             </div>
+            <div className='featuredCont'>
+                <div className='alignedProjects'>
+                    <div className='projectImage'>
+                        <p>left aligned image goes here</p>
+                    </div>
+                    <div className='projectText'>
+                        <p>Featured Project</p>
+                        <p>Project Name</p>
+                        <p>Text Box</p>
+                        <p>Technologies</p>
+                        <p>Icon Links</p>
+                    </div>
+                </div>
+                <div className='alignedProjects'>
+                    <div className='projectText'>
+                    <div className='projectText'>
+                        <p>Featured Project</p>
+                        <p>Project Name</p>
+                        <p>Text Box</p>
+                        <p>Technologies</p>
+                        <p>Icon Links</p>
+                    </div>
+                    </div>
+                    <div className='projectImage'>
+                        <p>right aligned image goes here</p>
+                    </div>
+                </div>
+                <div className='alignedProjects'>
+                    <div className='projectImage'>
+                        <p>left aligned image goes here</p>
+                    </div>
+                    <div className='projectText'>
+                    <div className='projectText'>
+                        <p>Featured Project</p>
+                        <p>Project Name</p>
+                        <p>Text Box</p>
+                        <p>Technologies</p>
+                        <p>Icon Links</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
