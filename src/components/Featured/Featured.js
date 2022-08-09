26063c49e4815304f3ce0d4b2729cec1ffa2244c
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-09 14:36:21 -0700

    added images to the featured component

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Featured.css'
 
 // Image imports
-import buhay from '../../images/buhay.jpg'
+import buhay from '../../images/buhaypomo.png'
+import apod from '../../images/apod.png'
+import pokemon from '../../images/pokemon.png'
 
 const Featured = () => {
     
@@ -13,8 +15,8 @@ const Featured = () => {
             </div>
             <div className='featuredCont'>
                 <div className='alignedProjects'>
-                    <div className='projectImage'>
-                        <img src={buhay} />
+                    <div className='projectImageCont'>
+                        <img src={buhay} className="projectImage"/>
                     </div>
                     <div className='projectText'>
                         <p>Featured Project</p>
@@ -25,29 +27,27 @@ const Featured = () => {
                     </div>
                 </div>
                 <div className='alignedProjects'>
-                    <div className='offsetText'>
+                    <div className='projectText'>
                         <p>Featured Project</p>
                         <p>Project Name</p>
                         <p>Text Box</p>
                         <p>Technologies</p>
                         <p>Icon Links</p>
                     </div>
-                    <div className='projectImage'>
-                        <p>right aligned image goes here</p>
+                    <div className='projectImageCont'>
+                        <img src={apod} className="projectImage"/>
                     </div>
                 </div>
                 <div className='alignedProjects'>
-                    <div className='projectImage'>
-                        <p>left aligned image goes here</p>
+                    <div className='projectImageCont'>
+                        <img src={pokemon} className="projectImage"/>
                     </div>
-                    <div className='projectText'>
                     <div className='projectText'>
                         <p>Featured Project</p>
                         <p>Project Name</p>
                         <p>Text Box</p>
                         <p>Technologies</p>
                         <p>Icon Links</p>
-                    </div>
                     </div>
                 </div>
             </div>
