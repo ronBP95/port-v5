2947e3e21b4eadf5a0786c9ea10e3b80fd8b7b56
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-11 00:37:58 -0700

    Created contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,14 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div id='contact'>
-            <div className='contactHeader'>04. What's Next?</div>
+            <div className='contactCont'>
+                <div className='contactText'>
+                    <h3 className='contactHeader'>04. What's Next?</h3>
+                    <h1>Get In Touch</h1>
+                    <p id="contactDesc">I am currently looking for Full Stack Engineering roles! Whether you have a question or you just want to say hello, the best way to reach me is through Linkedin!</p>
+                </div>
+                <a id='contactButton' href='https://www.linkedin.com/in/ron-magpantay/'>Say Hello</a>
+            </div>
         </div>
     )
 }
