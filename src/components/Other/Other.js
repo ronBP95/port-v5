2c62e1204159d3a7015bf493f1474701cd6de02e
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-11 15:49:16 -0700

    Scrapped the Other component to try again

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -1,62 +1,11 @@
-import {React, useState, useEffect} from 'react';
-import './Other.css'
+import React from 'react';
 
 const Other = () => {
-    
-    const [page, setPage] = useState(null);
-
-    const query = `{
-        jobsCollection {
-          items {
-            jobs {
-              title
-              description
-              contentType
-              fileName
-              size
-              url
-              width
-              height
-            }
-          }
-        }
-      }
-    `
-
-    console.log('space id is:' + `${process.env.YOUR_SPACE_ID}`)
-    
-    useEffect(() => {
-      window
-        .fetch(`https://graphql.contentful.com/content/v1/spaces/` + `${process.env.YOUR_SPACE_ID}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            // Authenticate the request
-            Authorization: "Bearer " + `${process.env.YOUR_ACCESS_TOKEN}`,
-          },
-          // send the GraphQL query
-          body: JSON.stringify({ query }),
-        })
-        .then((response) => response.json())
-        .then(({ data, errors }) => {
-          if (errors) {
-            console.error(errors);
-          }
-    
-          // rerender the entire component with new data
-          setPage(data.jobsCollection.items.[0]);
-        });
-    }, []);
-    
-    if (!page) {
-      return "Loading...";
-    }
-    
     return (
-        <div className='other'>
-            <p>Other</p>
+        <div>
+            <p>hi</p>
         </div>
-    )
+    );
 }
 
 export default Other;
