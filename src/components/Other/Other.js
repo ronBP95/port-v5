d18bf693da4ad494f6020699059001340627fc24
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-11 11:20:43 -0700

    setting up a query for contentful data

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import './Other.css'
+import contentful from 'contentful'
+
+let client = contentful.createClient({
+    space: '7fu5o07l6mxg',
+    accessToken: 'GM0x2kHdRRFiBWVLYn6LPiM89v94pHYZjOwO0MUEglo',
+})
+
+let pullData = client.getEntry('4M0RIEvv488xRnZaYtX2MC').then(function (entry) {
+    // logs the entry metadata
+    console.log(entry.sys);
+  
+    // logs the field with ID title
+    console.log(entry.fields.productName);
+});
+
+console.log(pullData)
 
 const Other = () => {
     return (
