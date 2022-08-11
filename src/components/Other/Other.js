1a6a3144ecebb1ad7fe4ed2d981ffc4acfbd0d5d
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-11 15:55:05 -0700

    Query worked and content was pulled but I need to set the process env variables

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import  React, { useState, useEffect } from "react";
+
+const query = `
+{
+  jobsCollection {
+    items {
+      title
+    }
+  }
+}
+`;
 
 const Other = () => {
+    const [jobs, setJobs] = useState(null);
+
+    useEffect(() => {
+    window
+        .fetch(`https://graphql.contentful.com/content/v1/spaces/` + `${process.env.YOUR_SPACE_ID}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + `${process.env.YOUR_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({ query }),
+        })
+        .then((response) => response.json())
+        .then(({ data, errors }) => {
+        if (errors) {
+            console.error(errors);
+        }
+
+        setJobs(data.jobsCollection.items[0]);
+        });
+    }, []);
+
+    if (!jobs) {
+        return "Loading...";
+    }
+
     return (
-        <div>
-            <p>hi</p>
+        <div className="App">
+          <header className="App-header">
+            <p>{jobs.title}</p>
+          </header>
         </div>
-    );
+      );
 }
 
 export default Other;
