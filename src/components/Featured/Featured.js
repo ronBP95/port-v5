66c8e151c116442716bf9b29d44d787c0d373378
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-11 21:01:51 -0700

    Created Footer component + deleted .env.development and will refresh keys

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -43,14 +43,12 @@ const Featured = () => {
                     <div className='offsetText'>
                         <div className='headerCont'>
                             <h4 className='projectHeader'>Featured Project</h4>
-                            <h2 className='projectName'>Project Name</h2>
+                            <h2 className='projectName'>Just Apod</h2>
                         </div>
                         <div className='offsetTextBox'>
-                            <p>Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
-                            Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, 
-                            this course covers everything from explaining the principles of REST APIs to implementing Spotify's 
-                            OAuth flow and fetching API data in a React app. By the end of the course, you’ll have an app deployed 
-                            to the internet you can add to your portfolio.</p>
+                            <p>just_Apod as it stands, is a simple application with the main purpose of this project 
+                            aimed at gaining experience working with Express and API's, which in this case, was 
+                            the NASA Open API called "Astronomy Picture of the Day" or APOD for short.</p>
                         </div>
                         <div className='offsetTech'>
                             <p>Express</p>
