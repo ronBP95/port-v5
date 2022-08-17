631c6a93e143833844a11ed65a9685e7353f0020
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-17 02:30:37 -0700

    Added href for resume

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,7 +14,7 @@ const Nav = () => {
                 <li className='navitems'><a href='#experience'> <span className='navspan'>02.</span> Experience</a></li>
                 <li className='navitems'><a href='#projects'><span className='navspan'>03.</span> Work</a></li>
                 <li className='navitems'><a href='#contact'><span className='navspan'>04.</span> Contact</a></li>
-                <a id="resumebutton" target='_blank'>Resume</a>
+                <a id="resumebutton" target='_blank' href="https://assets.ctfassets.net/7fu5o07l6mxg/7rv5EdQ1JE56WvBzqFawcF/75ef35de4a0e9be8f55b18d0722dddb4/Public_Resume.pdf">Resume</a>
             </ul>
             <div id="dropdown">
                 <p><GiHamburgerMenu /></p>
