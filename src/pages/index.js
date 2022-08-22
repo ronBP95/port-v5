de6e277db32549d7b1846a9530addf9d70953f71
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-21 21:53:47 -0700

    Changed path to see if that can resolve the ananalytics data

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ import Name from "../components/Name/Name"
 // React Google Analytics
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-223761393-1');
-ReactGA.pageview('/pages/index');
+ReactGA.pageview('/');
 
 const IndexPage = () => {
     return (
