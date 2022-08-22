253e2095d9d0226ddecfa802ed8b30cf4b765c2c
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-21 21:58:26 -0700

    Trying heap analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,9 +18,13 @@ import Social from "../components/Social/Social"
 import Name from "../components/Name/Name"
 
 // React Google Analytics
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-223761393-1');
-ReactGA.pageview('/');
+// import ReactGA from 'react-ga';
+const heapInit = () => {
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};   
+    heap.load("YOUR_APP_ID"); 
+}
+
+heapInit();
 
 const IndexPage = () => {
     return (
