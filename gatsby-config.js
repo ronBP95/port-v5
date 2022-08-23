f6e73cc69f39553960b29db9ff216f9a81a39af0
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-23 13:39:26 -0700

    Changed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,6 @@ module.exports = {
           // You can add multiple tracking ids and a pageview event will be fired for all of them.
           trackingIds: [
             "G-2M3Z1ZZ33L", // Google Analytics / GA
-            "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-            "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
           ],
           // This object gets passed directly to the gtag config command
           // This config will be shared across all trackingIds
@@ -27,7 +25,7 @@ module.exports = {
           // This object is used for configuration specific to this plugin
           pluginConfig: {
             // Puts tracking script in the head instead of the body
-            head: false,
+            head: true,
             // Setting this parameter is also optional
             respectDNT: true,
             // Avoids sending pageview hits from custom paths
