da4f63cadf3f885a30c741f24911afbd14dca5b7
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-23 13:45:49 -0700

    Removed most of the boilerplate for Gatsby plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,28 +11,8 @@ module.exports = {
       {
         resolve: `gatsby-plugin-google-gtag`,
         options: {
-          // You can add multiple tracking ids and a pageview event will be fired for all of them.
-          trackingIds: [
+          trackingIds: 
             "G-2M3Z1ZZ33L", // Google Analytics / GA
-          ],
-          // This object gets passed directly to the gtag config command
-          // This config will be shared across all trackingIds
-          gtagConfig: {
-            optimize_id: "OPT_CONTAINER_ID",
-            anonymize_ip: true,
-            cookie_expires: 0,
-          },
-          // This object is used for configuration specific to this plugin
-          pluginConfig: {
-            // Puts tracking script in the head instead of the body
-            head: true,
-            // Setting this parameter is also optional
-            respectDNT: true,
-            // Avoids sending pageview hits from custom paths
-            exclude: ["/preview/**", "/do-not-track/me/too/"],
-            // Defaults to https://www.googletagmanager.com
-            origin: "YOUR_SELF_HOSTED_ORIGIN",
-          },
         },
       },   
       {
