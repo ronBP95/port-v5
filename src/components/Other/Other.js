48bcf8e2a8270f036c1a49713dc38196ec347a1b
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-05 21:50:28 -0700

    Removed the useEffect in other component. Will explore other options for Other component project listing

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -1,48 +1,10 @@
 import  React, { useState, useEffect } from "react";
 import "./Other.css"
 
-const query = `
-{
-  jobsCollection {
-    items {
-      title
-    }
-  }
-}
-`;
-
 const Other = () => {
-    const [jobs, setJobs] = useState(null);
-    let token = `${process.env.GATSBY_YOUR_ACCESS_TOKEN}`
-    let space = `${process.env.GATSBY_YOUR_SPACE_ID}`
-
-    useEffect(() => {
-    window
-        .fetch(`https://graphql.contentful.com/content/v1/spaces/` + space.toString() , {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token.toString(),
-        },
-        body: JSON.stringify({ query }),
-        })
-        .then((response) => response.json())
-        .then(({ data, errors }) => {
-        if (errors) {
-            console.error(errors);
-        }
-
-        setJobs(data.jobsCollection.items[0]);
-        });
-    }, []);
-
-    if (!jobs) {
-        return "Loading...";
-    }
-
     return (
         <div className="other">
-            <p>{jobs.title}</p>
+            <p>Other Component</p>
         </div>
       );
 }
