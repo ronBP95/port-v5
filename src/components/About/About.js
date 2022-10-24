44834b2cad3b6fd1d985ffbf5392ad53852e6c37
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-24 00:57:00 -0700

    Major overhaul of the About.js component and media queries had been overwritten completely

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,52 +1,42 @@
 import React from 'react';
 import './About.css'
 import monke from '../../images/wow.webp'
-import { BiRightArrow } from 'react-icons/bi'
 
 const About = () => {
     return (
-        <div id='about'>
-            <div className="aboutwords">
-                <div className="aboutText">
-                    <div className='aboutHeader'>
-                        <p><span id="aboutnum">01.</span> About Me</p>
-                        <div id='sideline'></div>
-                    </div>
-                    
-                    <div className='aboutDesc'>
-                        <br></br>
-                        <p> Hello! My name is Ron Magpantay and I love building things on the web. My interest in web development began back in 2015 where I learned how to work with HTML + CSS to build a mock sales page for my favorite cars.</p>
-                        <br></br>
-                        <p> Fast-forward to today, I've learned how to code and build out websites and databases, provided development services for a non-profit organization, and interned for an engineering team at a multivitamin startup.</p>
-                    </div>
-                    
-                    <div className='techDesc'>
-                        <br></br>
-                        <p> Here are some of the technnologies I have been working with recently:</p>
-                        <br></br>
-                        <br></br>
-                        <div id="techlist">
-                            <ul className='aboutUl'>
-                                <li><BiRightArrow className='arrow'/> Javascript (ES6+)</li>
-                                <li><BiRightArrow className='arrow'/> Ruby</li>
-                                <li><BiRightArrow className='arrow'/> Gatsby</li>
-                            </ul>
-                            <ul className='aboutUl'>
-                                <li><BiRightArrow className='arrow'/> React</li>
-                                <li><BiRightArrow className='arrow'/> Express</li>
-                                <li><BiRightArrow className='arrow'/> Mongo</li>
-                            </ul>
-                        </div>
+        <div id="about">
+            <div className="aboutHeader">
+                <div><span id="aboutNum">01.</span> About Me</div>
+            </div>
+            <div className="aboutCont">
+                <div className="aboutDesc">
+                    <div>Hello! My name is <span id="aboutName">Ron Magpantay</span> and I love building things on the web. My interest in web development began back in 2015 where I learned how to work with HTML + CSS to build a mock sales page for my favorite cars.</div>
+                    <br></br>
+                    <div>Fast-forward to today, I've learned how to code and build out websites and databases, provided development services for a non-profit organization, and interned for an engineering team at a multivitamin startup.</div>
+                    <br></br>
+                    <div>I also recently completed an Ecommerce application called Big Store that does everything a an Ecommerce does ranging from Account Creation to Checking out and receiving a receipt.</div>
+                    <br></br>
+                    <div>Here are a few technologies I've been working with recently:</div>
+                    <br></br>
+                    <div className="technologies">
+                        <ul className="techList">
+                            <li>Javascript</li>
+                            <li>React</li>
+                            <li>Node.js</li>
+                        </ul>
+                        <ul className="techList">
+                            <li>TypeScript</li>
+                            <li>Material-UI</li>
+                            <li>Tailwind</li>
+                        </ul>
                     </div>
                 </div>
-
-                <div className='aboutpic'>
+                <div className="aboutPicture">
                     <img src={monke} id='monke'></img>
                 </div>
             </div>
-            <div id="experience"></div>
         </div>
-    )
+    );
 }
 
 export default About;
---
+++ src/components/About/AboutOld.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './About.css'
+import monke from '../../images/wow.webp'
+import { BiRightArrow } from 'react-icons/bi'
+
+const About = () => {
+    return (
+        <div id='about'>
+            <div className="aboutwords">
+                <div className="aboutText">
+                    <div className='aboutHeader'>
+                        <p><span id="aboutnum">01.</span> About Me</p>
+                        <div id='sideline'></div>
+                    </div>
+                    
+                    <div className='aboutDesc'>
+                        <br></br>
+                        <p> Hello! My name is Ron Magpantay and I love building things on the web. My interest in web development began back in 2015 where I learned how to work with HTML + CSS to build a mock sales page for my favorite cars.</p>
+                        <br></br>
+                        <p> Fast-forward to today, I've learned how to code and build out websites and databases, provided development services for a non-profit organization, and interned for an engineering team at a multivitamin startup.</p>
+                    </div>
+                    
+                    <div className='techDesc'>
+                        <br></br>
+                        <p> Here are some of the technnologies I have been working with recently:</p>
+                        <br></br>
+                        <br></br>
+                        <div id="techlist">
+                            <ul className='aboutUl'>
+                                <li><BiRightArrow className='arrow'/> Javascript (ES6+)</li>
+                                <li><BiRightArrow className='arrow'/> Ruby</li>
+                                <li><BiRightArrow className='arrow'/> Gatsby</li>
+                            </ul>
+                            <ul className='aboutUl'>
+                                <li><BiRightArrow className='arrow'/> React</li>
+                                <li><BiRightArrow className='arrow'/> Express</li>
+                                <li><BiRightArrow className='arrow'/> Mongo</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='aboutpic'>
+                    <img src={monke} id='monke'></img>
+                </div>
+            </div>
+            <div id="experience"></div>
+        </div>
+    )
+}
+
+export default About;
